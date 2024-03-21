efb9103c1b5c8ec43f43243a04287ab981e00e7f
Xiaolei Bi And <xbi@eamv.dk>
2024-03-21 19:20:06 +0100

    ak

--- 02-basic-concepts/01-goals/app.js
+++ 02-basic-concepts/01-goals/app.js
@@ -1,20 +1,21 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            courseGoalA: "Have fun and learn stuff",
-            courseGoalB: "Be the best coder in the whole wide world",
-            // courseGoalB: "<em>Be the best coder in the whole wide world</em>",
-            vueLink: 'https://vuejs.org',
-            message: 'You loaded this page on ' + new Date().toLocaleString()
-        }
-    },
-    methods: {
-        outputGoal() {
-            const randomNumber = Math.random()
-            return randomNumber < 0.5 ? this.courseGoalA : this.courseGoalB
-        }
-    }
-})
+	data() {
+		return {
+			courseGoalA: "Have fun and learn stuff",
+			courseGoalB: "Be the best coder in the whole wide world",
+			// courseGoalB: "<em>Be the best coder in the whole wide world</em>",
+			vueLink: "https://vuejs.org",
+			message: "You loaded this page on " + new Date().toLocaleString(),
 
-app.mount('#user-goal')
+			output: "Hello Vue!",
+		};
+	},
+	methods: {
+		outputGoal() {
+			const randomNumber = Math.random();
+			return randomNumber < 0.5 ? this.courseGoalA : this.courseGoalB;
+		},
+	},
+});
 
+app.mount("#user-goal");
